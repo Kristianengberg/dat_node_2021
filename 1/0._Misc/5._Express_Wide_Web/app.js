e6cca07b9964fa57b0a282b772fa6ecc556f7e2b
Kristian Engberg <krissoengberg@gmail.com>
2021-03-23 18:04:57 +0100

    added all

--- 1/0._Misc/5._Express_Wide_Web/app.js
+++ 1/0._Misc/5._Express_Wide_Web/app.js
@@ -4,8 +4,30 @@ const port = process.env.PORT || 8080;
 
 console.log(port);
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
+    res.sendFile(__dirname + "/public/welcome/index.html")
+});
+
+app.get("/pizza", (req, res) => {
+    res.sendFile(__dirname + "/public/pizza.html")
+});
+
+app.get("/endingwithredirect", (req, res) => {
+    res.send({});
+});
+
+app.get("/redirect", (req, res) => {
+    res.redirect("/endingwithredirect")
+});
+
+app.get("/candle", (req, res) => {
+    if (req.query.blow) {
+        res.send({ data: "lightsOn" });
+    } else {
+        res.send({ data: "lightsOff" });
+    }
 });
 
 app.listen(port, () => {
---
+++ 1/0._Misc/5._Express_Wide_Web/public/welcome/welcome.js
@@ -0,0 +1,5 @@
+console.log("Welcome");
+
+$("#pizza-oven").click(function() {
+    $(this).html("o");
+});
